73c90d3026d6567ab4545f089a875ef98af9fcd8
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-16 22:38:16 +0900

    updaet: confetti

---
+++ pages/posted.js
@@ -0,0 +1,73 @@
+// @flow
+
+import * as React from "react";
+import { type Dispatch } from "redux";
+import styled from "styled-components";
+import { connect } from "react-redux";
+import { Waypoint } from "react-waypoint";
+import Confetti from "react-confetti";
+import Card from "../components/Card";
+import Text from "../components/common/Text";
+import Header from "../components/Header";
+import { actions } from "../redux/modules/card";
+import { type Store } from "../redux/modules";
+import { type TCards } from "../typedef/api/cards";
+import { type TError } from "../typedef/api/error";
+import COLOR from "../constatns/color";
+
+type MapStateToProps = {|
+  +isLoading: boolean,
+  +isLoaded: boolean,
+  +data: TCards | null,
+  +error: TError | null,
+  +cursor: number
+|};
+
+type MapDispatchToProps = {|
+  +startFetchData: typeof actions.startFetchData
+|};
+
+type Props = {|
+  ...MapStateToProps,
+  ...MapDispatchToProps
+|};
+
+class Posted extends React.Component<Props> {
+  componentDidMount() {}
+
+  render() {
+    return (
+      <Wrapper>
+        <Confetti width={500} height={1000} />
+        <Inner>
+          <Text>投稿ありがとうございました！</Text>
+        </Inner>
+      </Wrapper>
+    );
+  }
+}
+
+const Wrapper = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${COLOR.background};
+  height: 100%;
+`;
+
+const Inner = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 80%;
+  height: 80%;
+  background: ${COLOR.white};
+`;
+
+const mapStateToProps = (state: Store): MapStateToProps => ({});
+
+const mapDispatchToProps = (dispatch: Dispatch<any>) => ({});
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Posted);
