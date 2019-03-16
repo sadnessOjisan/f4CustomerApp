a5b3e2eb2712896eafc401ff11063fa0cda99b3e
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-16 14:20:38 +0900

    update: webpack

--- next.config.js
+++ next.config.js
@@ -2,19 +2,11 @@ require("dotenv").config();
 const webpack = require("webpack");
 module.exports = {
   webpack: (config, { dev }) => {
-    config.plugins.push(new webpack.EnvironmentPlugin(process.env));
-    new webpack.DefinePlugin({
-      "process.env": {
-        REACT_APP_ENV: JSON.stringify(process.env.REACT_APP_ENV)
-      }
-    });
-    config.devtool = "source-map";
-    for (const options of config.plugins) {
-      if (options["constructor"]["name"] === "UglifyJsPlugin") {
-        options.options.sourceMap = true;
-        break;
-      }
-    }
+    config.plugins.push(
+      new webpack.DefinePlugin({
+        "process.env.NODE_ENV": JSON.stringify(process.env.NODE_ENV)
+      })
+    );
     return config;
   },
   publicRuntimeConfig: () => {
