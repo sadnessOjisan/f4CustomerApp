9c70bd4fa175edf412ed38811d25485313087ee3
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-16 21:51:17 +0900

    update: post message

--- redux/sagas/index.js
+++ redux/sagas/index.js
@@ -1,9 +1,10 @@
 import { fork, all } from "redux-saga/effects";
 import cardSaga from "./cardSaga";
 import employeeSaga from "./employeeSaga";
+import messagePostSaga from "./messagePostSaga";
 
 function* rootSaga() {
-  yield all([fork(cardSaga), fork(employeeSaga)]);
+  yield all([fork(cardSaga), fork(employeeSaga), fork(messagePostSaga)]);
 }
 
 export default rootSaga;
