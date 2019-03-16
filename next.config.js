dfc0c4a83e0cba25b0dede08cf7329129e04f70b
sadness_ojisan <sadness.ojisan@gmail.com>
2019-03-16 13:36:04 +0900

    update: webpack line

--- next.config.js
+++ next.config.js
@@ -1,4 +1,5 @@
 require("dotenv").config();
+const webpack = require("webpack");
 module.exports = {
   webpack: (config, { dev }) => {
     config.plugins.push(new webpack.EnvironmentPlugin(process.env));
